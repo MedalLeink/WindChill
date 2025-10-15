ee2e93cd878336fafacf3a37ac230ca8880d6eed
Madeleine Nkiru <anaghamadeleine@gmail.com>
2025-10-15 15:28:23 -0400

    function to convert celsius to fahrenheit

--- windchill.py
+++ windchill.py
@@ -7,6 +7,14 @@
 def calculate_windchill(temp_f, wind_speed):
 
     """Calculate and return wind chill in Fahrenheit."""
-    
+
     wind_chill = 35.74 + (0.6215 * temp_f) - (35.75 * (wind_speed ** 0.16)) + (0.4275 * temp_f * (wind_speed ** 0.16))
     return wind_chill
+
+
+# Function to convert Celsius to Fahrenheit
+def celsius_to_fahrenheit(temp_c):
+
+    """Convert Celsius temperature to Fahrenheit."""
+    
+    return (temp_c * 9/5) + 32
