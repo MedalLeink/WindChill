84447710443c9745171793fe301d4ca4ec4fb7ce
Madeleine Nkiru <anaghamadeleine@gmail.com>
2025-10-15 15:23:44 -0400

    function to calculate windchill

---
+++ windchill.py
@@ -0,0 +1,12 @@
+# Windchill Calculator
+# By: Nkiru Madeleine Anagha
+
+
+
+# Function to calculate wind chill
+def calculate_windchill(temp_f, wind_speed):
+
+    """Calculate and return wind chill in Fahrenheit."""
+    
+    wind_chill = 35.74 + (0.6215 * temp_f) - (35.75 * (wind_speed ** 0.16)) + (0.4275 * temp_f * (wind_speed ** 0.16))
+    return wind_chill
